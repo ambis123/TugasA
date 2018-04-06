ff3adcb7f17e948b26b7f335612c14b3c550550e
ambis123 <38097225+ambis123@users.noreply.github.com>
2018-04-06 22:45:18 +0700

    Membuat Atribut

--- src/tugasa/TugasA1.java
+++ src/tugasa/TugasA1.java
@@ -10,5 +10,21 @@ package tugasa;
  * @author Muhammad Rizaldy
  */
 public class TugasA1 {
+     String Nama;
+    String NPM;
+    String Kelas;
+    String Class;
+    String Atribut;
+    String Behavior;
+    String Objek;
     
+    void cetakInfo(){
+            System.out.println("Nama : "+Nama);
+            System.out.println("NPM : "+NPM);
+            System.out.println("Kelas : "+Kelas);
+            System.out.println("Class : "+Class);
+            System.out.println("Objek : "+Objek);
+            System.out.println("Atribut : "+Atribut);
+            System.out.println("Behavior : "+Behavior);
+    }
 }
