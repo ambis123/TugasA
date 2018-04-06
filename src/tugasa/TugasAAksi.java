fdf42af4ed5f336e1516bce71af2b604e00dc031
ambis123 <38097225+ambis123@users.noreply.github.com>
2018-04-06 22:54:46 +0700

    Mengisi

--- src/tugasa/TugasAAksi.java
+++ src/tugasa/TugasAAksi.java
@@ -12,5 +12,18 @@ package tugasa;
 public class TugasAAksi {
     public static void main(String[] args) { 
         TugasA1 Abdi = new TugasA1(); 
+        
+        Abdi.Nama="Abdi Khanafy";
+            Abdi.NPM="16630929";
+            Abdi.Kelas="4A Nonreg";
+            Abdi.Class="Rumah";
+            Abdi.Objek="Rumah Om, Rumah Kakek, Rumah Ayah";
+            Abdi.Atribut="Rumah Om=Warna Dinding:merah,Warna genteng:Merah"
+                                 +"Bahan Dinding:Beton, Bahan Lantai :Batu Kramik ";
+            Abdi.Atribut="Rumah Kakek=Warna Dinding:Biru,Warna genteng:Biru"
+                                 +"Bahan Dinding:Kayu Ulin, Bahan Lantai :Kayu ";
+            Abdi.Atribut="Rumah Ayah=Warna Dinding:Coklat,Warna genteng:Coklat"
+                                 +"Bahan Dinding:Kasiboat, Bahan Lantai :Kayu Ulin ";
+            Abdi.Behavior="Melindungi dari Hujan dan Panas";
 }
 }
